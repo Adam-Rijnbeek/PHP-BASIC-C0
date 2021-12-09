654d6d837e032b5bf911ac7ca26bd921251e6c68
Adam <adam.rijnbeek@student.rocva.nl>
2021-12-09 10:21:26 +0100

    php 4.3

--- 2-Variabelen-en-datatype/2.4 - Datatype boolean & null/index.php
+++ 2-Variabelen-en-datatype/2.4 - Datatype boolean & null/index.php
@@ -4,21 +4,10 @@
 
     
 <?php
-$getal1 = 10;
-$getal2 = 19;
-echo $getal1 + $getal2; 
+$getal = 69;
+echo $getal++;
 echo "<br>";
-$getal3 = 5;
-$getal4 = 5;
-echo $getal3 * $getal4;
-echo "<br>";
-$getal5 = 20;
-$getal6 = 8;
-echo $getal5 - $getal6;
-echo "<br>";
-$getal7 = 15;
-$getal8 = 3;
-echo $getal7 / $getal8;
+echo $getal--;
 ?>
 
 </body>

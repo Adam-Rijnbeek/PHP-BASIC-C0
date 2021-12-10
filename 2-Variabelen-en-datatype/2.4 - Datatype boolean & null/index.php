1bae2904b602a0ab725c17bc3d83055b2d7b61f3
Adam <adam.rijnbeek@student.rocva.nl>
2021-12-10 12:48:21 +0100

    php 5.2

--- 2-Variabelen-en-datatype/2.4 - Datatype boolean & null/index.php
+++ 2-Variabelen-en-datatype/2.4 - Datatype boolean & null/index.php
@@ -4,23 +4,22 @@
 
     
 <?php
-$test1 = 12;
-$test2 = "gewoon tekst";
-var_dump($test1 == $test2);
+$test1 = 10;
+$test2 = 6;
+if($test1 == $test2); 
+    echo "Ongelijk";
 echo "<br>";
-var_dump($test1 === $test2);
+
+if($test1 == $test2);
+    echo "Identiek";
 echo "<br>";
-var_dump($test1 != $test2);
+
+if($test1 == $test2);
+    echo "test1 is groter dan test2";
 echo "<br>";
-var_dump($test1 > $test2);
-echo "<br>";
-var_dump($test1 < $test2);
-echo "<br>";
-var_dump($test1 <> $test2);
-echo "<br>";
-var_dump($test1 >= $test2);
-echo "<br>";
-var_dump($test1 <= $test2);
+
+if($test1 == $test2);
+    echo "test1 is groter of gelijk aan test2";
 
 
 ?>

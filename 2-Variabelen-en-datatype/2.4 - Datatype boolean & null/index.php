e7a1730d571ef63db0edaeac4a391a68186d9f75
Adam <adam.rijnbeek@student.rocva.nl>
2021-12-14 10:33:45 +0100

    php 7.1

--- 2-Variabelen-en-datatype/2.4 - Datatype boolean & null/index.php
+++ 2-Variabelen-en-datatype/2.4 - Datatype boolean & null/index.php
@@ -4,13 +4,6 @@
 
     
 <?php
-function geefTienProcent(){
-    $koopPrijs = 75;
-    $korting = 10;
-    $kortingEuro = ($koopPrijs / 100) * $korting;
-    return $koopPrijs - $kortingEuro;
-}
-echo geefTienProcent();
 
 
 

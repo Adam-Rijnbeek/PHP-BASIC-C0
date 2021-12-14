f2d3579eef844098706f0096b1d175db6c067cd6
Adam <adam.rijnbeek@student.rocva.nl>
2021-12-14 09:27:10 +0100

    php 5.6

--- 2-Variabelen-en-datatype/2.4 - Datatype boolean & null/index.php
+++ 2-Variabelen-en-datatype/2.4 - Datatype boolean & null/index.php
@@ -4,24 +4,42 @@
 
     
 <?php
-$score = 0;
+$var1 = 3;
 
-switch($score){
-    case 10:
-    echo "Wow wat een fantastische score!";
-    break;
-    case 8:
-    echo "Wow bijna alle punten nog 1 te gaan!";
-    break;
-    case 6:
-    echo "Goed bezig, bijna alle punten!";
-    break;
-    case 4:
-    echo "Ga zo door!";
-    break;
-    default: 
-    echo "Nog even oefenen dan haal je vast alle punten!";
+if($var1 < 10 && $var1 > 5) {
+    echo "het getal zit tussen de 10 en de 5!";
+} else {
+    echo "het getal is groter dan 10 of kleiner dan 5!";
 }
+echo "<br>";
+$var2 = 4;
+
+if($var2 < 12 || $var2 > 3) {
+    echo "het getal zit tussen de 12 en de 3!";
+} else {
+    echo "het getal is groter dan 12 of kleiner dan 3!";
+}
+echo "<br>";
+$var3 = 7;
+
+if($var3 < 20 xor $var3 > 5) {
+    echo "het getal zit tussen de 20 en de 8!";
+} else {
+    echo "het getal is groter dan 20 of kleiner dan 8!";
+}
+echo "<br>";
+$var4 = 10;
+
+if($var4 < 30 != $var4 > 12) {
+    echo "het getal zit tussen de 30 en de 12!";
+} else {
+    echo "het getal is groter dan 30 of kleiner dan 12!";
+}
+
+
+
+
+
 
 
 

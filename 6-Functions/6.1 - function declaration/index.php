7fce7a842dfdca300a72ac9ed02ceff7179d0963
Adam <adam.rijnbeek@student.rocva.nl>
2022-01-27 15:01:05 +0100

    6.1 af

---
+++ 6-Functions/6.1 - function declaration/index.php
@@ -0,0 +1,8 @@
+<?php
+$woordje = "Hallo";
+$zinnetje = "mooie blauwe planeet!";
+function ZetTweeWoordenAanElkaar($woordje, $zinnetje){
+    $helezin = $woordje. " " .$zinnetje;
+    echo $helezin;
+    ?>
+    

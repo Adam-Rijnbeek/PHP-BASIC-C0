7d39e3df62f1e376dcc48eb47413efdf3aa990e7
Adam <adam.rijnbeek@student.rocva.nl>
2022-01-27 14:54:56 +0100

    5.3 af

---
+++ 5-Conditional_Statements/5.3 - if, else conditional/index.php
@@ -0,0 +1,25 @@
+<?php
+$test1 = 3;
+$test2 = 6;
+echo "gelijk";
+echo "<br>";
+echo "false";
+echo "<br>";
+if($test1 == $test2);
+echo "true";
+echo "<br>";
+if($test1 == $test2);
+echo "false";
+echo "<br>";
+echo "true";
+echo "<br>";
+if($test1 == $test2);
+echo "false";
+echo "<br>";
+if($test1 == $test2);
+echo "true";
+
+
+
+
+?>

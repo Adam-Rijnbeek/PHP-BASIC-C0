5d3d3dc468c75a193023c005acb9bcac3fd36f7f
Adam <adam.rijnbeek@student.rocva.nl>
2022-01-27 11:48:33 +0100

    2.4 af

--- 2-Variabelen-en-datatype/2.4 - Datatype boolean & null/index.php
+++ 2-Variabelen-en-datatype/2.4 - Datatype boolean & null/index.php
@@ -2,18 +2,11 @@
 <html>
 <body>
 
-    
 <?php
-
-
-
-
-
-
-
-
-
-
+$test = false;
+echo "de waarde van test :" . $test . "<br>";
+$test = true;
+echo "de waarde van deze test is:" . $test . "<br>";
 ?>
 
 </body>

6bb7a8103cf75003ac76b90846fac75cadfb2424
Adam <adam.rijnbeek@student.rocva.nl>
2022-01-27 12:27:16 +0100

    3.3 af

---
+++ 3-Arrays/3.3 - Array waarde toevoegen/index.php
@@ -0,0 +1,11 @@
+<?php
+$myHeroes =  ['Spiderman', 'Thor', 'Iron man'];
+echo $myHeroes[0] = 'Spiderman';
+echo "<br>";
+echo $myHeroes[1] = 'Thor';
+echo "<br>";
+echo $myHeroes[2] = 'Iron man';
+array_push($myHeroes, 'Hulk' );
+print_r($myHeroes);
+
+?>

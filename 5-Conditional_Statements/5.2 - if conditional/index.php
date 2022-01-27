1308f1ff1971a8d540f71b910b29f7bcc6162de4
Adam <adam.rijnbeek@student.rocva.nl>
2022-01-27 14:49:20 +0100

    5.2 af

---
+++ 5-Conditional_Statements/5.2 - if conditional/index.php
@@ -0,0 +1,17 @@
+<?php
+$test1 = 10;
+$test2 = 6;
+if($test1 == $test2); 
+    echo "Ongelijk";
+    echo "<br>";
+    if($test1 == $test2);
+    echo "Identiek";
+echo "<br>";
+if($test1 == $test2);
+    echo "test1 is groter dan test2";
+echo "<br>";
+if($test1 == $test2);
+    echo "test1 is groter of gelijk aan test2";
+
+
+?>
